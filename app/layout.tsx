ee28672a7d0737ee5a9d35e744fb294e75dd1d3d
RawdhaMahmoud <rawdhamahmoud4@gmail.com>
2026-01-15 20:20:54 +0100

    update portfolio title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,22 @@
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+import "./globals.css";
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+export const metadata = {
+  title: "Rawdha Mahmoud ",
+  description: "Portfolio of Rawdha Mahmoud, IT student",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html>
-      <body>
-        <header>Navbar</header>
-        {children}
-        <footer>Footer</footer>
+    <html lang="en" className="scroll-smooth">
+      <body className="antialiased">
+        <Header />
+        <main className="pt-16">{children}</main>
+        <Footer />
       </body>
     </html>
   );
